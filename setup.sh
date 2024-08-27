cd6e0fd6b7550b1ba1bc03706400f514f7ea5a55
Josh <63868275+jerseyjosh@users.noreply.github.com>
2024-08-27 10:21:34 +0100

    Update setup.sh
    think it works?

--- setup.sh
+++ setup.sh
@@ -1,30 +1,39 @@
-# install packages
+#!/bin/bash
+
+# Install necessary packages
 apt update && apt install -y zsh screen htop python3-venv
 
-# git setup
+# Git configuration
 git config --global user.email "josh@hakuna.co.uk"
 git config --global user.name "Josh Griffiths"
 
-# install ohmyzsh
+# Install Oh My Zsh
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" bash --unattended
 
-# Set zsh as the default shell
+# Set Zsh as the default shell
 chsh -s $(which zsh)
 
-# Setup python venv
+# Setup Python virtual environment
 if [ -d "/home/josh" ]; then
-    # use /home/josh if it exists
+    # Use /home/josh if it exists
     HOME_DIR="/home/josh"
 else
-    # otherwise default to /home
+    # Otherwise default to /home
     HOME_DIR="/home"
 fi
-cd "$HOME_DIR" || exit 1  # cd into home directory, exit if cd fails
+
+cd "$HOME_DIR" || exit 1  # Change to home directory, exit if it fails
+
+# remake venv if exists
 if [ -d "venv" ]; then
     rm -rf venv
 fi
+
+# activate environment
 python3 -m venv venv
 source venv/bin/activate
+
+# Install Python packages
 python3 -m pip install --upgrade pip \
     numpy \
     pandas \
@@ -38,6 +47,7 @@ python3 -m pip install --upgrade pip \
     polars \
     pyarrow \
     cvxpy
+
 python3 -m ipykernel install --user --name=venv --display-name="Python (venv)"
 
 # Generate SSH key for GitHub to clone repos
@@ -49,9 +59,10 @@ eval "$(ssh-agent -s)"
 # Add your SSH private key to the ssh-agent
 ssh-add ~/.ssh/id_ed25519
 
-# install istari repos
-mkdir -p packages # make packages dir if not exists
+# Install Istari repos
+mkdir -p packages  # Create packages directory if it doesn't exist
 cd packages
+
 repos=(
     "git@github.com:istari-capital/quant-tools.git"
     "git@github.com:istari-capital/dataframe-tools.git"
@@ -59,24 +70,26 @@ repos=(
     "git@github.com:istari-capital/preprocessing.git"
     "git@github.com:istari-capital/spread-research.git"
 )
-# Loop through the repository URLs and clone them in parallel
+
+# Clone repositories in parallel
 for repo in "${repos[@]}"; do
     git clone "$repo" &
 done
+
 # Wait for all background jobs to finish
 wait
 
-# Loop through each new packages and install
+# Install each new package
 for dir in */; do
     if [ -d "$dir" ]; then
         python3 -m pip install "$dir"
     fi
 done
 
-# change zsh defualts
+# Change Zsh defaults
 echo "source $HOME_DIR/venv/bin/activate" >> /root/.zshrc
 
-# start jupyter server in a detached screen session
+# Start Jupyter server in a detached screen session
 screen -dmS jupyter zsh -c "cd $HOME_DIR && jupyter notebook --allow-root --no-browser --port=8888 --NotebookApp.token='' --NotebookApp.password=''"
 
 # Print SSH public key
@@ -84,4 +97,4 @@ echo "---------"
 echo "SSH key generated. Please add the following public key to GitHub:"
 cat ~/.ssh/id_ed25519.pub
 echo "---------"
-echo "Logout and log back in to activate zsh if not already activated."
+echo "Logout and log back in to activate Zsh if not already activated."
